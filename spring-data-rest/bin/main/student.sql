035b14cb7088dcefd6f23821a679cd76df56f0f7
kk <kk@DESKTOP-FVU0OLT.mshome.net>
2018-12-29 13:04:55 +0530

    first code changes

---
+++ spring-data-rest/bin/main/student.sql
@@ -0,0 +1,3 @@
+insert into student values(101,'bhaskar','Madanapalli')
+insert into student values(102,'abcd','agl')
+insert into student values(103,'mno','agl')
---
+++ spring-data-rest/src/main/java/com/springdata/springdatarest/repository/StudentRepo.java
@@ -0,0 +1,11 @@
+package com.springdata.springdatarest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.springdata.springdatarest.model.Student;
+
+@RepositoryRestResource(collectionResourceRel="students",path="students")
+public interface StudentRepo extends JpaRepository<Student, Integer>{
+
+}
